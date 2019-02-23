8b94645099cef8094a8f0be0043e9386ef877120
nicolas munoz <nicolas.munoz.2019@upb.edu.co>
2019-02-23 11:01:36 -0500

    Solucion

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-a=int(input("digite el numero que desee"))
-c=int(input("digite otro numero de interes:V"))
-DobleA=(2*a)
-producto=(a*c)
-print(("el doble de A es: "), DobleA)
-print(("el producto de a y c es: "), producto)
+import math
+
+a=int(input("ingresar valor a"))
+b=int(input("ingresar valor b"))
+c=int(input("ingresar valor c"))
+
+d=b*b-4*a*c
+
+if a!=0:
+	if d<0:
+		print("entonces no existe solución en los números reales")
+	else:
+		x1=(-b+(math.sqrt(d)))/(2*a)
+		x2=(-b-(math.sqrt(d)))/(2*a)
+		print("x1 = "+str(x1)+" x2 = "+str(x2))
+else:
+	print("coeficioente a debe ser diferente de cero")
